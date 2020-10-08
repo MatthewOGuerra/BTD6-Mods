9b8757df945345a38c6d3bb2e8830b26069a85d2
Matt <39469614+KosmicShovel@users.noreply.github.com>
2020-10-08 12:28:38 -0500

    Update HyperSonic_init.py

--- BTD6.py/Examples/HyperSonic_init.py
+++ BTD6.py/Examples/HyperSonic_init.py
@@ -1,10 +1,10 @@
 # Have init in the file name for it to only run once
-convert
+util
 
 for tower in gameModel.towers:
     for behavior in tower.behaviors:
         if 'AttackModel' in behavior.name:
-            attackModel = convert.BehaviorToAttackModel(behavior)
+            attackModel = util.BehaviorToAttackModel(behavior)
             if len(attackModel.weapons) > 0:
                 attackModel.weapons[0].rateFrames = 0
                 attackModel.weapons[0].rate = 0
