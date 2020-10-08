50d988c4f7be94f95db14c21d158713f9461b657
Matt <39469614+KosmicShovel@users.noreply.github.com>
2020-10-08 12:07:30 -0500

    Create PyEngine.cs

---
+++ BTD6.py/Src/PyEngine.cs
@@ -0,0 +1,26 @@
+
+using System.Dynamic;
+using Assets.Scripts.Models;
+using Assets.Scripts.Unity.UI_New.InGame;
+using BTD6.py.Utils;
+using IronPython.Hosting;
+
+namespace BTD6.py
+{
+    class PyEngine : DynamicObject
+    {
+        public static void RunScript(string s, GameModel model)
+        {
+            var engine = Python.CreateEngine();
+            var scope = engine.CreateScope();
+            scope.SetVariable("gameModel", model);
+            scope.SetVariable("inGame", InGame.instance);
+            scope.SetVariable("convert", new Convert());
+
+            var source = engine.CreateScriptSourceFromFile(s);
+            var compiled = source.Compile();
+
+            compiled.Execute(scope);
+        }
+    }
+}
