009454c8412f52a3b14bebab11ef8556640f0255
Matt <39469614+KosmicShovel@users.noreply.github.com>
2020-10-12 09:55:41 -0500

    Update Util.cs

--- BTD6.py/Src/Utils/Util.cs
+++ BTD6.py/Src/Utils/Util.cs
@@ -1,14 +1,21 @@
 
+using System.Linq;
 using Assets.Scripts.Models;
 using Assets.Scripts.Models.Towers.Behaviors.Attack;
+using UnhollowerBaseLib;
 
-namespace BTD6.py.Utils
+namespace BTD6.Script.Utils
 {
     public class Util
     {
         public AttackModel BehaviorToAttackModel(Model m)
         {
             return m.Cast<AttackModel>();
+        }
+
+        public Model FirstOrDefault(Il2CppArrayBase<Model> models, string name)
+        {
+            return models.FirstOrDefault(tmp => tmp.name.Contains(name));
         }
 
         //TODO   more.
